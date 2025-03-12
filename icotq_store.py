68cfc0a10dc33aa45899dd425cd813f5a9f7c09b
Dominik Schlösser <dominik.schloesser@gmail.com>
2025-03-12 17:40:30 +0100

    incmpl

--- icotq_store.py
+++ icotq_store.py
@@ -17,12 +17,20 @@ class IcotqConfig(TypedDict):
     ebook_mirror: str
 
 
+class LibEntry(TypedDict):
+    source_name: str
+    filename: str
+    desc_filename: str
+    text: str
+
+
 class IcoTqStore:
     def __init__(self) -> None:
         self.log:logging.Logger = logging.getLogger("IcoTqStore")
         config_path = os.path.expanduser("~/.config/icotq")  # Turquoise icosaeder
         if os.path.isdir(config_path) is False:
             os.makedirs(config_path)
+        self.lib: list[LibEntry] = []
         config_file = os.path.join(config_path, "icoqt.json")
         self.config:IcotqConfig
         if os.path.exists(config_file):
@@ -30,7 +38,6 @@ class IcoTqStore:
                 iqc: IcotqConfig = json.load(f)
                 self.config = iqc
         else:
-
             self.config = IcotqConfig({
                 'icotq_path': '~/IcoTqStore',
                 'tq_sources': [
@@ -51,13 +58,13 @@ class IcoTqStore:
             with open(config_file, 'w') as f:
                 json.dump(self.config,f)
             self.log.warning(f"Created default configuration at {config_file}, please review!")
-        root_path = os.path.expanduser(self.config['icotq_path'])
-        if os.path.exists(root_path) is False:
-            os.makedirs(root_path)
-            self.log.warning(f"Creating IcoTq storage path at {root_path}, all IcoTq data will reside there. Modify {config_file} to chose another location!")
+        self.root_path:str = os.path.expanduser(self.config['icotq_path'])
+        if os.path.exists(self.root_path) is False:
+            os.makedirs(self.root_path)
+            self.log.warning(f"Creating IcoTq storage path at {self.root_path}, all IcoTq data will reside there. Modify {config_file} to chose another location!")
         config_subdirs = ['Texts', 'Embeddings', 'PDFTextCache', 'EpubTextCache']
         for cdir in config_subdirs:
-            full_path = os.path.join(root_path, cdir)
+            full_path = os.path.join(self.root_path, cdir)
             if os.path.isdir(full_path) is False:
                 os.makedirs(full_path)
         for source in self.config['tq_sources']:
@@ -78,7 +85,70 @@ class IcoTqStore:
             if valid is False:
                 self.config['tq_sources'].remove(source)
                 self.log.warning(f"Please fix configuration file {config_file}")
+        self.read_library()
         
     def list_sources(self) -> None:
         for id, source in enumerate(self.config['tq_sources']):
             print(f"{id:02d} {source['name']}, {source['tqtype']}, {source['path']}, {source['file_types']}")
+
+    def read_library(self):
+        lib_path = os.path.join(self.root_path, "icotq_library.json")
+        if os.path.exists(lib_path) is True:
+            with open(lib_path, 'r') as f:
+                self.lib = json.load(f)
+        else:
+            self.log.info(f"No current library state at {lib_path}")
+
+    def write_library(self):
+        lib_path = os.path.join(self.root_path, "icotq_library.json")
+        with open(lib_path, 'w') as f:
+            json.dump(self.lib, f)
+
+    def import_texts(self):
+        if len(self.config['tq_sources']) == 0:
+            self.log.error(f"No valid sources defined in config, can't import")
+            return
+        lib_changed = False
+        for source in self.config['tq_sources']:
+            source_path = os.path.expanduser(source['path'])
+            for root, _dir, files in os.walk(source_path):
+                for file in files:
+                    ext = os.path.splitext(file)[1]
+                    if len(ext) > 0:
+                        ext = ext[1:]  # remove leading '.'
+                    if ext not in source['file_types']:
+                        continue
+                    full_path = os.path.join(root, file)
+                    desc_path = "{"+ source['name'] + "}" + full_path[len(source_path):]
+                    in_lib = False
+                    for entry in self.lib:
+                        if entry['desc_filename'] == desc_path:
+                            # Check if changed!
+                            in_lib = True
+                            break
+                    if in_lib is False:
+                        text = None
+                        if ext in ['md', 'py', 'txt']:
+                            with open(full_path, 'r') as f:
+                                text = f.read()
+                        elif ext == 'pdf':
+                            print("can't do PDF yet")
+                            continue
+                        else:
+                            self.log.error(f"Unsupported converstion {ext} to text")
+                            continue
+                        entry: LibEntry = LibEntry({
+                            'source_name': source['name'],
+                            'desc_filename': desc_path,
+                            'filename': full_path,
+                            'text': text
+                        })
+                        self.lib.append(entry)
+                        lib_changed = True
+        if lib_changed is True:
+            self.write_library()
+            self.log.info("Changed library saved.")
+
+            
+
+    
--- iq.py
+++ iq.py
@@ -1,14 +1,35 @@
 import logging
+import os
 import argparse
 from argparse import ArgumentParser
-from icotq_store import IcoTqStore
+from icotq_store import IcoTqStore, TqSource
 from typing import cast
 
-def iq_info(its: IcoTqStore) -> None:
+def iq_info(its: IcoTqStore, logger:logging.Logger) -> None:
     its.list_sources()
 
+def iq_export(its: IcoTqStore, logger:logging.Logger) -> None:
+    if 'ebook_mirror' not in its.config:
+        logger.error(f"Cannot export, destination 'ebook_mirror' not defined in config")
+        return
+    ebook_mirror_path = os.path.expanduser(its.config['ebook_mirror'])
+    if os.path.isdir(ebook_mirror_path) is False:
+        logger.error(f"Destination directory {ebook_mirror_path} does not exist, aborting export!")
+        return
+    print(f"Export to {ebook_mirror_path}")
+
+def iq_import(its: IcoTqStore, logger:logging.Logger):
+    its.import_texts()
+
+def iq_help(parser:argparse.ArgumentParser, valid_actions:list[str]):
+    parser.print_help()
+    print()
+    print("Command can either be provided as command-line arguments or at the '> ' prompt.")
+    print("Valid commands are: " + ', '.join(valid_actions))
+    print("To exit, simply press Enter at the command prompt, or by 'exit' or 'quit'")
+
 def parse_cmd(its: IcoTqStore, logger: logging.Logger) -> None:
-    valid_actions = ['info', 'export']
+    valid_actions = ['info', 'export', 'import', 'help']
     parser: ArgumentParser = argparse.ArgumentParser(description="IcoTq")
     _ = parser.add_argument(
         "action",
@@ -16,18 +37,47 @@ def parse_cmd(its: IcoTqStore, logger: logging.Logger) -> None:
         default="",
         help="Action: " + ', '.join(valid_actions),
     )
+    _ = parser.add_argument(
+            "-n",
+            "--non-interactive",
+            action="store_true",
+            help="Non-interactive mode, do not enter repl",
+        )        
     args = parser.parse_args()
-    actions: list[str] =  cast(list[str], args.action)
-    for action in actions:
-        if action not in valid_actions:
-            logger.error(f"Invalid action {action}, valid are: {valid_actions}")
-            exit(1)
-    if 'info' in actions:
-        iq_info(its)
+    quit:bool = False
+    while quit is False:
+        actions: list[str] =  cast(list[str], args.action)
+        for action in actions:
+            if action not in valid_actions:
+                logger.error(f"Invalid action {action}, valid are: {valid_actions}")
+                exit(1)
+        if 'info' in actions:
+            iq_info(its, logger)
+        if 'export' in actions:
+            iq_export(its, logger)
+        if 'import' in actions:
+            iq_import(its, logger)
+        if 'help' in actions:
+            iq_help(parser, valid_actions)
+        if cast(bool, args.non_interactive) is True:
+            break
+        try:
+            cmd = input("> ")
+        except (EOFError, KeyboardInterrupt):
+            quit = True
+            continue
+        print(f"{len(cmd)}: >{cmd}<")
+        cmd = cmd.strip().replace('  ', ' ')
+        if cmd == "" or cmd == 'quit' or cmd == 'exit': 
+            quit = True
+        else:
+            args = parser.parse_args(cmd.split(' '))
+    print()
 
 def main() -> None:
     logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger("IQ")
+    logger.info("Starting...")
      
     its = IcoTqStore()
     parse_cmd(its, logger)
