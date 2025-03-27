83dff470a2f36044a92f84e781d81e8264d8edca
Dominik Schlösser <dominik.schloesser@gmail.com>
2025-03-27 21:59:56 +0100

    remove emb srch

--- ebook_tools.py
+++ ebook_tools.py
@@ -7,7 +7,6 @@ import json
 import argparse
 from argparse import ArgumentParser
 from typing import TypedDict, cast, Any
-from rich.console import Console
 
 from calibre_tools import CalibreTools
 from kindle_tools import KindleTools
@@ -15,7 +14,6 @@ from md_tools import MdTools, MDTable
 from indra_tools import IndraTools
 from metadata import Metadata
 from time_lines import TimeLines
-from ai_search import HuggingfaceEmbeddings, SearchResult
 
 
 class ConfigDict(TypedDict):
@@ -67,7 +65,7 @@ if __name__ == "__main__":
         "action",
         nargs="*",
         default="",
-        help="Action: export, notes, kindle, indra, meta, timeline, bookdates, embed, search",
+        help="Action: export, notes, kindle, indra, meta, timeline, bookdates",
     )
     _ = parser.add_argument(
         "-t",
@@ -134,8 +132,6 @@ if __name__ == "__main__":
     do_meta: bool = "meta" in action
     do_timeline: bool = "timeline" in action
     do_bookdates: bool = "bookdates" in action
-    do_embed: bool = "embed" in action
-    do_search: bool = "search" in action
     do_date_stuff: bool = (do_bookdates is True or do_timeline is True)
     use_sha256 = cast(bool, args.SHA256)
 
@@ -150,8 +146,6 @@ if __name__ == "__main__":
         and do_indra is False
         and do_meta is False
         and do_bookdates is False
-        and do_embed is False
-        and do_search is False
     ):
         logger.error("No action specified, exiting, use -h for help")
         exit(1)
@@ -178,20 +172,9 @@ if __name__ == "__main__":
         with open(config_file, "w") as f:
             _ = f.write(json.dumps(default_config, indent=4))
         logger.info(f"Config file {config_file} created, please edit it")
-        default_model: EmbeddingsModel = {
-             "model_name": "nomic-ai/nomic-embed-text-v2-moe",
-             "chunk_size": 2048,
-             "chunk_overlap": 2048 // 3,
-        }
         txt_data = os.path.expanduser(default_config['book_text_lib'])
         os.makedirs(os.path.join(txt_data, "Texts"), exist_ok=True)
         os.makedirs(os.path.join(txt_data, "PDF_Texts"), exist_ok=True)
-        embs = os.path.join(txt_data, "embeddings")
-        os.makedirs(embs, exist_ok=True)
-        model_file = os.path.join(embs, 'model.json')
-        with open(model_file, "w") as f:
-             _ = f.write(json.dumps(default_model, indent=4))
-        logger.info(f"Model file {model_file} created, please edit it")
         exit(1)
     else:
         try:
@@ -210,19 +193,6 @@ if __name__ == "__main__":
     book_text_lib_embeddings: str = os.path.join(book_text_lib_root, "embeddings")
     book_text_lib_texts: str = os.path.join(book_text_lib_root, "Texts")
     book_text_lib_pdf_texts: str = os.path.join(book_text_lib_root, "PDF_Texts")
-    model_file = os.path.join(book_text_lib_embeddings, 'model.json')
-    model_config: EmbeddingsModel = {
-         "model_name": "nomic-ai/nomic-embed-text-v2-moe",
-         "chunk_size": 2048,
-         "chunk_overlap": 2048 // 3
-         }
-    if os.path.exists(model_file) is True:
-         with open(model_file, "r") as f:
-              model_config = json.load(f)
-    else:
-        with open(model_file, "w") as f:
-            json.dump(model_config, f)
-            logging.warning(f"Created default model config file: {model_file}")
 
     paths = [calibre_path, kindle_path, meta_path, notes_path, book_text_lib_root]
     for p in paths:
@@ -370,108 +340,6 @@ if __name__ == "__main__":
                         m_oks += 1
                         # print(meta)
         logger.info(f"Processed metadata, ok={m_oks}, errors={m_errs}")
-    if do_embed is True:
-        if emb is None:
-             emb = HuggingfaceEmbeddings(repository = book_text_lib_root, embeddings_model_name=model_config['model_name'], chunk_size=model_config['chunk_size'], chunk_overlap=model_config["chunk_overlap"])
-             emb.load_state()
-        book_cnt = 0
-        # book_cnt += emb.add_texts(library_name="TempRecip", source_folder="~/Temp/Rezepte", formats=["md"])
-        # logging.info(f"Book_cnt: {book_cnt}, self.texts is {len(emb.texts.keys())}")
-        # book_cnt += emb.add_texts(library_name="TempPDF", source_folder="~/Temp/PDF", formats=["pdf"])
-        # logging.info(f"Book_cnt: {book_cnt}, self.texts is {len(emb.texts.keys())}")
-        book_cnt += emb.add_texts(library_name="CalNotes", source_folder=notes_path, formats=["md"])
-        book_cnt += emb.add_texts(library_name="CalText", source_folder=book_text_lib_texts, formats=["txt"])
-        book_cnt += emb.add_texts(library_name="CalPdf", source_folder=meta_path, formats=["pdf"])
-        emb.generate_embeddings()
-        emb.save_state()
-        logger.info("Text embeddings processed")
-    if do_search is True:
-        search_spec = cast(str, args.keywords)
-        if search_spec == "":
-            logger.error("Please specify a search-string with `-k` parameter")
-            exit(1)
-        if emb is None:
-            logger.info("Loading embeddings...")
-            emb = HuggingfaceEmbeddings(repository = book_text_lib_root, embeddings_model_name=model_config['model_name'], chunk_size=model_config['chunk_size'], chunk_overlap=model_config["chunk_overlap"])
-            emb.load_state()
-        max_results = 15
-        context = 16
-        context_steps = 4
-        yellow = True
-        cols, _ = os.get_terminal_size()
-        results: list[SearchResult] | None = emb.search(search_text=search_spec, yellow_liner=yellow, context=context, context_steps=context_steps, max_results=max_results, compress="full")
-        print()
-        print()
-        console = Console()
-        if len(results) > 0:
-            for i in range(len(results)):
-                result = results[i]
-                y_min: float | None = None
-                y_max: float | None = None
-                ryel = result['yellow_liner']
-                # print(ryel)
-                yels: list[float] = []
-                if ryel is not None:
-                    if len(ryel.shape) == 1:
-                        lyel = ryel.tolist()
-                        yels = cast(list[float], lyel)
-                        for y in yels:
-                             if y_min is None or y<y_min:
-                                  y_min = y
-                             if y_max is None or y>y_max:
-                                  y_max = y
-                    else:
-                        logger.error(f"Yellow-liner result has wrong shape: {ryel.shape}")
-                        continue
-                else:
-                    print(results[i]['chunk'])
-                if y_min == None:
-                     y_min = 0
-                if y_max == None:
-                     y_max = 1
-                title_text = f"Document: {result['desc']}[{result['index']}], {result['cosine'] * 100.0:2.1f} %"
-                if len(title_text) < cols: 
-                    title_text += " " * (cols - len(title_text))
-                else:
-                    title_text = title_text[:cols]
-                console.print("[#FFFFFF on #D0D0D0]"+"-"*cols+"[/]")
-                console.print("[black on #E0E0E0]"+title_text+"[/]")
-                console.print("[#FFFFFF on #E0E0E0]"+"-"*cols+"[/]")
-                # sys.stdout.flush()
-                # print(best_chunk)
-                # print(y_min, y_max)
-                if y_min == y_max:
-                     print(f"Search gave no meaningful result: y_min: {y_min}, y_max: {y_max}, search-embedding vector is trivial (language not supported?)")
-                     print(result['chunk'])
-                     continue
-                if yels != []:
-                    line = ""
-                    char_ind = 0
-                    for i, c in enumerate(result['chunk']):
-                        y_ind = i//context_steps
-                        if y_ind >= len(yels):
-                            print(f"Index out of range: {y_ind}, len(yels): {len(yels)}")
-                        else:
-                            yel:float = (yels[y_ind]-y_min)/(y_max - y_min)
-                            if yel < 0.5:
-                                yel = 0.0
-                            col = hex(255 - int(yel*127.0))[2:]
-                            if c == "\n":
-                                rest = "[black on #FFFFFF]" + " " * (cols - char_ind%cols) + "[/]"
-                                line += rest
-                                char_ind = 0
-                            else:
-                                line += f"[black on #FFFF{col}]"+c+"[/]"
-                                if ord(c)>31:
-                                    char_ind += 1
-                    if char_ind > 0:
-                        rest = "[black on #FFFFFF]" + "-" * (cols - char_ind%cols) + "[/]"
-                        line += rest
-                        char_ind = 0
-                    console.print(line, soft_wrap=True)
-            console.print("[#FFFFFF on #E0E0E0]"+"-"*cols+"[/]")
-        else:
-            print("No search result available!")
         
     if do_bookdates is True:
         logger.error(f"Can't access the book library texts at {book_text_lib_root}")
