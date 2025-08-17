a3dde16d8980cf4adf960cb2902972dd3571d0ca
Dominik Schlösser <dominik.schloesser@gmail.com>
2025-08-17 10:04:17 +0200

    Don´t crash on emacs temp files

--- md_tools.py
+++ md_tools.py
@@ -456,7 +456,7 @@ class MdTools:
                     dirs.remove(skip_dir)
             for file in files:
                 num += 1
-                if file.endswith(".md"):
+                if file.endswith(".md") and not file.startswith(".#"):
                     if progress is True:
                         notes_progress += 1
                         progress_str = progress_bar_string(
